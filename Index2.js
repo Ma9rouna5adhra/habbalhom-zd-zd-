edeccf4a04542e1a0a25ab863d1730d8a901fd40
Ma9rouna5adhra <41198838+Ma9rouna5adhra@users.noreply.github.com>
2018-07-13 16:43:36 +0200

    Add files via upload

---
+++ Index2.js
@@ -0,0 +1,19 @@
+function function1(){
+     
+        $("#lol").toggle();
+    }
+    function function2(){
+        document.getElementById("lolo").innerHTML= prompt("Tell Me Your Darkest Secrets")
+    }
+    function function3(){
+         $("#lol2").toggle();
+        }
+        function function4(){
+             $("#lol1").toggle();
+            }
+            function function5(){
+                 $("#img").toggle();
+                }
+                function function6(){
+                $("#vid").toggle();
+                    }
